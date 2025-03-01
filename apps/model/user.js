314387502c05e37e9fe57d5cb23ccc5b546fe0aa
khenhdicode <khenhdicode1907@gmail.com>
2025-03-01 10:55:05 +0700

    create responsitory

---
+++ apps/model/user.js
@@ -0,0 +1,16 @@
+class User {
+    id;
+    name;
+    constructor(){
+    }
+    
+    tonghaiso(a,b) {
+        return a+b;
+    }
+    
+    static tonghaisov1(a,b) {
+        return a+b;
+    }
+}
+
+module.exports = User;
